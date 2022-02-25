78c4e0e97f6b333272a6e99920cacfc891277bc0
its-madhumitha <93436575+its-madhumitha@users.noreply.github.com>
2022-02-25 15:47:12 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+<script>
+        var navlinks= document.getElementById("navLinks")
+        function showMenu(){
+          navlinks.style.left=0
+        }
+        function hideMenu(){
+          navlinks.style.left=-200
+        }
+    </script>
